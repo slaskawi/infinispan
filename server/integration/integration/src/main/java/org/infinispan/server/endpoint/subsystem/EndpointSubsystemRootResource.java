7c1af42ee12be6655ef406087c3ba5f984ae6bff
Tristan Tarrant <ttarrant@redhat.com>
2013-12-10 15:23:25 +0100

    JDG: Remove the WebSocket endpoint

--- server/integration/integration/src/main/java/org/infinispan/server/endpoint/subsystem/EndpointSubsystemRootResource.java
+++ server/integration/integration/src/main/java/org/infinispan/server/endpoint/subsystem/EndpointSubsystemRootResource.java
@@ -60,7 +60,6 @@ public class EndpointSubsystemRootResource extends SimpleResourceDefinition {
         resourceRegistration.registerSubModel(new HotRodConnectorResource(isRuntimeRegistration()));
         resourceRegistration.registerSubModel(new MemcachedConnectorResource(isRuntimeRegistration()));
         resourceRegistration.registerSubModel(new RestConnectorResource(isRuntimeRegistration()));
-        resourceRegistration.registerSubModel(new WebSocketConnectorResource(isRuntimeRegistration()));
     }
 
     public boolean isRuntimeRegistration() {
