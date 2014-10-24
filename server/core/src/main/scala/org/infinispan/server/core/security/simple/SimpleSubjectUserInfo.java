71a593582796a8369dd7f28871c7434051be9659
Tristan Tarrant <ttarrant@redhat.com>
2014-10-24 18:21:36 +0100

    BZ1156318 ISPN-4882 Fix HotRod authentication with clustered containers by wrapping calls to RpcManager and DistributionManager in security actions

--- server/core/src/main/scala/org/infinispan/server/core/security/simple/SimpleSubjectUserInfo.java
+++ server/core/src/main/scala/org/infinispan/server/core/security/simple/SimpleSubjectUserInfo.java
@@ -1,10 +1,14 @@
 package org.infinispan.server.core.security.simple;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.security.Principal;
 import java.util.Collection;
 
 import javax.security.auth.Subject;
 
+import org.infinispan.commons.marshall.SerializeWith;
 import org.infinispan.server.core.security.SubjectUserInfo;
 
 /**
@@ -13,6 +17,7 @@ import org.infinispan.server.core.security.SubjectUserInfo;
  * @author Tristan Tarrant
  * @since 7.0
  */
+@SerializeWith(SimpleSubjectUserInfo.Externalizer.class)
 public class SimpleSubjectUserInfo implements SubjectUserInfo {
     final String userName;
     final Subject subject;
@@ -22,7 +27,8 @@ public class SimpleSubjectUserInfo implements SubjectUserInfo {
        this.subject = subject;
     }
 
-    public String getUserName() {
+    @Override
+   public String getUserName() {
         return userName;
     }
 
@@ -34,5 +40,20 @@ public class SimpleSubjectUserInfo implements SubjectUserInfo {
    @Override
    public Subject getSubject() {
       return subject;
+   }
+
+   public static class Externalizer implements org.infinispan.commons.marshall.Externalizer<SimpleSubjectUserInfo> {
+
+      @Override
+      public void writeObject(ObjectOutput output, SimpleSubjectUserInfo object) throws IOException {
+         output.writeObject(object.userName);
+         output.writeObject(object.subject);
+      }
+
+      @Override
+      public SimpleSubjectUserInfo readObject(ObjectInput input) throws IOException, ClassNotFoundException {
+         return new SimpleSubjectUserInfo((String)input.readObject(), (Subject)input.readObject());
+      }
+
    }
 }
