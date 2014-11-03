f92229733a219f47fc9eee3a9d66c543b662041e
Dan Berindei <dan@infinispan.org>
2014-11-03 16:51:22 +0200

    BZ1156332 ISPN-4873 Statetransfer thread pool deadlock
    StateRequestCommands should not block a thread
    if they arrive before the REBALANCE_START command.

--- core/src/main/java/org/infinispan/remoting/InboundInvocationHandlerImpl.java
+++ core/src/main/java/org/infinispan/remoting/InboundInvocationHandlerImpl.java
@@ -1,10 +1,6 @@
 package org.infinispan.remoting;
 
-import org.infinispan.commands.CancellableCommand;
-import org.infinispan.commands.CancellationService;
-import org.infinispan.commands.CommandsFactory;
-import org.infinispan.commands.ReplicableCommand;
-import org.infinispan.commands.TopologyAffectedCommand;
+import org.infinispan.commands.*;
 import org.infinispan.commands.remote.CacheRpcCommand;
 import org.infinispan.commands.remote.MultipleRpcCommand;
 import org.infinispan.commands.remote.SingleRpcCommand;
@@ -23,13 +19,14 @@ import org.infinispan.factories.scopes.Scope;
 import org.infinispan.factories.scopes.Scopes;
 import org.infinispan.interceptors.totalorder.RetryPrepareException;
 import org.infinispan.remoting.responses.CacheNotFoundResponse;
-import org.infinispan.statetransfer.StateTransferLock;
-import org.infinispan.statetransfer.StateTransferManager;
 import org.infinispan.remoting.responses.ExceptionResponse;
 import org.infinispan.remoting.responses.Response;
 import org.infinispan.remoting.responses.ResponseGenerator;
 import org.infinispan.remoting.transport.Address;
 import org.infinispan.remoting.transport.Transport;
+import org.infinispan.statetransfer.StateRequestCommand;
+import org.infinispan.statetransfer.StateTransferLock;
+import org.infinispan.statetransfer.StateTransferManager;
 import org.infinispan.transaction.TotalOrderRemoteTransactionState;
 import org.infinispan.transaction.totalorder.TotalOrderLatch;
 import org.infinispan.transaction.totalorder.TotalOrderManager;
@@ -156,14 +153,23 @@ public class InboundInvocationHandlerImpl implements InboundInvocationHandler {
          });
       } else {
          final StateTransferLock stateTransferLock = cr.getStateTransferLock();
+          // Command topology id -1 means the command is not topology-aware
+          final int commandTopologyId = extractCommandTopologyId(cmd);
          // Always wait for the first topology (i.e. for the join to finish)
-         final int commandTopologyId = Math.max(extractCommandTopologyId(cmd), 0);
+         final int waitTopologyId = Math.max(commandTopologyId, 0);
+         // For regular topology-affected commands, we need to wait for the transaction data.
+         // For StateRequestCommands, that would lead to a deadlock, instead we only wait for the topology.
+         final boolean waitForTransactionData = !(cmd instanceof StateRequestCommand);
 
          if (!preserveOrder && cmd.canBlock()) {
             remoteCommandsExecutor.execute(new BlockingRunnable() {
                @Override
                public boolean isReady() {
-                  return stateTransferLock.transactionDataReceived(commandTopologyId);
+                  if (waitForTransactionData) {
+                     return stateTransferLock.transactionDataReceived(waitTopologyId);
+                  } else {
+                     return stateTransferLock.topologyReceived(waitTopologyId);
+                  }
                }
 
                @Override
@@ -185,9 +191,13 @@ public class InboundInvocationHandlerImpl implements InboundInvocationHandler {
                }
             });
          } else {
-            // Non-OOB commands. We still have to wait for transaction data, but we should "never" time out
-            // In non-transactional caches, this just waits for the topology to be installed
-            stateTransferLock.waitForTransactionData(commandTopologyId, 1, TimeUnit.DAYS);
+            if (waitForTransactionData) {
+               // Non-OOB commands. We still have to wait for transaction data, but we should "never" time out
+               // In non-transactional caches, this just waits for the topology to be installed
+               stateTransferLock.waitForTransactionData(waitTopologyId, 1, TimeUnit.DAYS);
+            } else {
+               stateTransferLock.waitForTopology(waitTopologyId, 1, TimeUnit.DAYS);
+            }
 
             if (0 < commandTopologyId && commandTopologyId < stm.getFirstTopologyAsMember()) {
                if (trace) log.tracef("Ignoring command sent before the local node was a member " +
